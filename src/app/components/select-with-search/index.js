865984b1a54b8b69ce8d06e126e2878c3c6c1bef
a.gontareva <a.gontareva@fun-box.ru>
2023-09-24 21:06:01 +0400

    (WIP)

--- src/app/components/select-with-search/index.js
+++ src/app/components/select-with-search/index.js
@@ -8,8 +8,13 @@ class SelectWithSearch extends HTMLElement {
     const templateContent = template.cloneNode(true).content;
 
     this.content = templateContent;
+    this.options = [];
 
-    this.setOptions(['Свекла', 'Морковь', 'Мясо'], templateContent);
+    this.setOptions([
+      { id: 1, label: 'Свекла' },
+      { id: 2, label: 'Морковь' },
+      { id: 3, label: 'Мясо' },
+    ], templateContent);
 
     const addButton = templateContent.querySelector('.select__add-button');
     // addButton.onclick = this.addOption();
@@ -21,31 +26,59 @@ class SelectWithSearch extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(templateContent);
 
-    this.hideButton = shadowRoot.querySelector('.select__container'); 
+    this.popup = shadowRoot.querySelector('.select__container');
+    this.input = shadowRoot.querySelector('.select__input');
+    this.selectContainer = shadowRoot.querySelector('.select__container');
+
   }
 
   // TODO отсоединить lister после отсоединения страницы
 
-  setOptions = options => {
-    const selectContainer = this.content.querySelector('.select__container');
-    selectContainer.innerHTML = null;
+  setOptions = (options, templateContent) => {
+    const popup = templateContent.querySelector('.select__container');
+    popup.innerHTML = null;
 
-    options.forEach(option => {
-      const optionNode = document.createElement('div');
-      optionNode.className = 'select__option';
-      optionNode.innerHTML = option;
+    options.forEach(option => this.setOption(option, popup));
+  }
 
-      selectContainer.appendChild(optionNode);
-    });
+  setOption = ({ id, label }, popup) => {
+    const optionNode = document.createElement('div');
+    optionNode.className = 'select__option';
+    optionNode.innerHTML = label;
+    optionNode.id = id;
+    optionNode.addEventListener('click', this.selectOption);
+
+    popup.appendChild(optionNode);
+
+    this.options = [...this.options, { id, label }];
   }
 
   addOption = () => {
+    const newValue = this.input.value;
+
+    if (!newValue || this.options.some(({ label }) => label === newValue)) return;
+
     this.text = `text: ${this.getAttribute('id')}`;
     this.dispatchEvent(new CustomEvent('addOption'));
+
+    const newId = Math.max(...this.options.map(({ id }) => id)) + 1;
+
+    this.setOption({ id: newId, label: newValue }, this.popup);
+
+    // // TODO передать во все выпадающие списки
+
+    if (!this.popup.classList.contains('hide')) {
+      this.togglePopup();
+    }
   }
 
   togglePopup = () => {
-    this.hideButton.classList.toggle('hide');
+    this.popup.classList.toggle('hide');
+  }
+
+  selectOption = event => {
+    this.input.value = event.currentTarget.innerHTML;
+    this.togglePopup();
   }
 }
 
